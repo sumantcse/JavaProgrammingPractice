220ad8f8aed5b550fa3cb08fdcedd2ce26df9f21
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 13:22:00 +0530

    Java Program Conditional Statements

---
+++ src/basic/ConditionalStatement.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		int numbers = 0;
+		if(numbers == 0) {
+			System.out.println("The number is zero");
+			}else if(numbers < 0) {
+				System.out.println("The number is negative");
+			}else
+				System.out.println("The number is positive");
+	}
+
+}
