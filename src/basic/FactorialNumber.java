1dedd3cf513fb6175df65bee26fc47a1bc70e3cc
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 22:02:51 +0530

    Program of Factorial Number

---
+++ src/basic/FactorialNumber.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		int  num = 5;
+		int fact = 1;
+		for(int i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+		System.out.print("Factorial of "+ num +" is "+ fact);
+
+	}
+
+}
