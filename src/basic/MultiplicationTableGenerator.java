a38885448028338b10c47bc2635d72765d9fdf1d
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 20:40:02 +0530

    Program for Multiplication Table Generator

---
+++ src/basic/MultiplicationTableGenerator.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class MultiplicationTableGenerator {
+
+	public static void main(String[] args) {
+		int num = 5;
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(num+" x "+ i + " = "+(num*i));
+		}
+
+	}
+
+}
