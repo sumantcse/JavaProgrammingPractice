ec80e873eae8f7bcd549daf32299cddd963d8419
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 12:44:32 +0530

    Java program Operators Expressions

---
+++ src/basic/OperatorsExpressionsPractice.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class OperatorsExpressionsPractice {
+
+	public static void main(String[] args) {
+		int num1 = 15;
+		int num2 = 4;
+		int sum =  num1 + num2;
+		int difference = num1 - num2;
+		int product = num1 * num2;
+		float Quotient = (float)num1 / num2;
+		int remainder = num1 % num2;
+		System.out.println("Sum: "+sum);
+		System.out.println("Difference: "+ difference);
+		System.out.println("Product: "+product);
+		System.out.println("Quotient:"+Quotient);
+		System.out.println("Reminder: "+remainder);
+
+	}
+
+}
