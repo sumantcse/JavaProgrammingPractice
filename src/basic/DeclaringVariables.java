e2031f80645376e7c9551ab3756067da822bc7a8
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 11:21:30 +0530

    Testing Variable declaration

---
+++ src/basic/DeclaringVariables.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class DeclaringVariables {
+
+	public static void main(String[] args) {
+		String name = "Sumant Pandey";
+		int age = 25;
+		double height = 172.2;
+		System.out.println("My name is "+name+".");
+		System.out.println("I am "+age+" years old.");
+		System.out.println("My height is "+height+" cm.");
+
+	}
+
+}
