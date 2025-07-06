63873bea11d01eb419f4d5e5b22a1b946f529f87
Sumant Pandey <sumantcse17@gmail.com>
2025-07-06 18:06:57 +0530

    Java pogram for Day of the week

---
+++ src/basic/SwitchStatementPractice.java
@@ -0,0 +1,37 @@
+package basic;
+
+import java.util.Scanner;
+
+public class SwitchStatementPractice {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		switch(num) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.print("Invalid day number");
+		}
+	}
+
+}
